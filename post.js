b43717d5b29b81838ed0fd34aeb79368c9022a05
firedev0x <memecoingpt@gmail.com>
2025-11-18 05:22:01 +0200

    Update post.js

--- post.js
+++ post.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { TwitterApi } from 'twitter-api-v2';
-import { generateGokeLine } from './generate.js';
+import { generateTweetAndImage } from './grok-image.js'; // noul tău fișier
 
 const client = new TwitterApi({
   appKey: process.env.X_APP_KEY,
@@ -9,12 +9,32 @@ const client = new TwitterApi({
   accessSecret: process.env.X_ACCESS_SECRET,
 });
 
+// conexiune media v1.1
+const rwClient = client.readWrite;
+
 try {
-  const text = await generateGokeLine();
-  if (!text || text.length < 5) throw new Error('draft gol sau prea scurt');
-  const res = await client.v2.tweet(text);
+  // 1) generăm tweet + imagine
+  const referenceImagePath = './assets/character.png'; // schimbă cu imaginea ta
+  const { tweetText, imagePath } = await generateTweetAndImage(referenceImagePath);
+
+  if (!tweetText || tweetText.length < 5) {
+    throw new Error('tweet gol sau prea scurt');
+  }
+
+  console.log('Tweet text:', tweetText);
+  console.log('Imagine generată:', imagePath);
+
+  // 2) încărcăm imaginea în Twitter
+  const mediaId = await rwClient.v1.uploadMedia(imagePath);
+
+  // 3) publicăm tweet-ul cu imagine
+  const res = await rwClient.v2.tweet({
+    text: tweetText,
+    media: { media_ids: [mediaId] }
+  });
+
   console.log('OK → Tweet ID:', res?.data?.id);
-  console.log('Text:', text);
+
 } catch (err) {
   console.error('Eroare:', err?.data ?? err?.message ?? err);
 }
